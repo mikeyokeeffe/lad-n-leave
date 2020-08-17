9e07971ad00484c52585c173a10ac1030e86f0d8
mikeyokeeffe <mikey.okeeffe@unipart.io>
2020-08-17 09:59:21 +0100

    Minor commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
